609a91f4fa6591edfb7afc6ddc741069f66b033c
Eric Phillips <eric@ewpt3ch.com>
2026-04-24 12:48:20 -0600

    CH7: cdn

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -126,7 +126,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		ContentType: &mediaType,
 	})
 
-	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+	videoURL := fmt.Sprintf("https://%s/%s", cfg.s3CfDistribution, key)
 	dbVideo.VideoURL = &videoURL
 	err = cfg.db.UpdateVideo(dbVideo)
 	if err != nil {
